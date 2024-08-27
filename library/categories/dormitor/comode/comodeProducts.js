27f4d3b229d4643d48db08c78cec4fb24352c38b
Erhard Nagy <Erson1989x@gmail.com>
2024-08-27 12:56:33 +0300

    in-progress

---
+++ library/categories/dormitor/comode/comodeProducts.js
@@ -0,0 +1,357 @@
+import comodaGeneva1S2D from "../../../../app/assets/dormitor/comode/Geneva1S2D/Comoda Geneva 1S2D.jpg"
+import comodaGeneva1S2D_1 from "../../../../app/assets/dormitor/comode/Geneva1S2D/Comoda Geneva 1S2D-2.jpg"
+import comodaGeneva1S2D_2 from "../../../../app/assets/dormitor/comode/Geneva1S2D/Comoda Geneva 1S2D-3.png"
+import comodaGeneva1S3D from "../../../../app/assets/dormitor/comode/Geneva1S3D/Comoda Geneva 1s3d.jpg"
+import comodaGeneva1S3D_1 from "../../../../app/assets/dormitor/comode/Geneva1S3D/Comoda Geneva 1S3D-2.jpg"
+import comodaGeneva3S from "../../../../app/assets/dormitor/comode/Geneva3S/Comoda Geneva 3S.jpg"
+import comodaGeneva3S_1 from "../../../../app/assets/dormitor/comode/Geneva3S/Comoda Geneva 3S-2.jpg"
+import comodaGeneva3S_2 from "../../../../app/assets/dormitor/comode/Geneva3S/Comoda Geneva 3S-3.jpg"
+import comodaFlora from "../../../../app/assets/dormitor/comode/Flora/Comoda Flora.jpg"
+import comodaFlora_1 from "../../../../app/assets/dormitor/comode/Flora/Comoda Flora-2.jpg"
+import comodaFlora_2 from "../../../../app/assets/dormitor/comode/Flora/Comoda Flora-3.webp"
+import comodaAresAS3 from "../../../../app/assets/dormitor/comode/AresAS3/Comoda ARES AS3.jpg"
+import comodaAresAS3_1 from "../../../../app/assets/dormitor/comode/AresAS3/Comoda ARES AS3-2.jpg"
+import comodaAresAS6 from "../../../../app/assets/dormitor/comode/AresAS6/Comoda ARES AS6.jpg"
+import comodaAresAS6_1 from "../../../../app/assets/dormitor/comode/AresAS6/Comoda ARES AS6-2.jpg"
+import comodaAresAS6_2 from "../../../../app/assets/dormitor/comode/AresAS6/Comoda ARES AS6-3.jpg"
+import comodaDisney900 from "../../../../app/assets/dormitor/comode/Disney900/Comoda Disney 900.jpg"
+import comodaDisney2D4S from "../../../../app/assets/dormitor/comode/Disney2D4S/Comoda Disney 2D4S.jpg"
+import comodaDisney2D4S_1 from "../../../../app/assets/dormitor/comode/Disney2D4S/Comoda Disney 2D4S-2.jpg"
+import comodaUK80 from "../../../../app/assets/dormitor/comode/UK80/Comoda UK 80.jpg"
+import comodaUK120 from "../../../../app/assets/dormitor/comode/UK120/Comoda UK120.jpg"
+import comodaUK120_1 from "../../../../app/assets/dormitor/comode/UK120/Comoda UK120-2.jpg"
+import comodaNika from "../../../../app/assets/dormitor/comode/Nika/Comoda Nika.jpg"
+import comodaVenera3S from "../../../../app/assets/dormitor/comode/Venera3S/Comoda Venera 3S.jpg"
+import comodaVenera3S_1 from "../../../../app/assets/dormitor/comode/Venera3S/Comoda Venera 3S-2.jpg"
+import comodaVenera3S2D from "../../../../app/assets/dormitor/comode/Venera3S2D/Comoda Venera 3S2D.jpg"
+import comodaVenera3S2D_1 from "../../../../app/assets/dormitor/comode/Venera3S2D/Comoda Venera 3S2D-2.jpg"
+import comodaVenera3S2D_2 from "../../../../app/assets/dormitor/comode/Venera3S2D/Comoda Venera 3S2D-3.jpg"
+import comodaTVBollo from "../../../../app/assets/dormitor/comode/Bollo/Comoda TV Bollo.jpg"
+import comodaAlma1U3S from "../../../../app/assets/dormitor/comode/Alma1U3S/Comoda Alma 1U3S.webp"
+import comodaAlma1U3S_1 from "../../../../app/assets/dormitor/comode/Alma1U3S/Comoda Alma 1U3S-2.webp"
+import comodaAlma1U3S_2 from "../../../../app/assets/dormitor/comode/Alma1U3S/Comoda Alma 1U3S-3.webp"
+import comodaAlma4S from "../../../../app/assets/dormitor/comode/Alma4S/Comoda Alma 4S.webp"
+import comodaAlma4S_1 from "../../../../app/assets/dormitor/comode/Alma4S/Comoda Alma 4S-2.webp"
+import comodaAlma4S_2 from "../../../../app/assets/dormitor/comode/Alma4S/Comoda Alma 4S-3.webp"
+import comodaAlma4S_3 from "../../../../app/assets/dormitor/comode/Alma4S/Comoda Alma 4S-4.webp"
+import comodaAlma2U3S from "../../../../app/assets/dormitor/comode/Alma2U3S/Comoda Alma 2U3S.webp"
+import comodaAlma2U3S_1 from "../../../../app/assets/dormitor/comode/Alma2U3S/Comoda Alma 2U3S-2.webp"
+import comodaAlma2U3S_2 from "../../../../app/assets/dormitor/comode/Alma2U3S/Comoda Alma 2U3S-3.webp"
+import comodaAlma2U3S_3 from "../../../../app/assets/dormitor/comode/Alma2U3S/Comoda Alma 2U3S-4.webp"
+import comodaLuxor from "../../../../app/assets/dormitor/comode/Luxor/Comoda Luxor.webp"
+import comodaLuxor_1 from "../../../../app/assets/dormitor/comode/Luxor/Comoda Luxor-2.webp"
+import comodaLuxor_2 from "../../../../app/assets/dormitor/comode/Luxor/Comoda Luxor-3.webp"
+import comodaKentuki from "../../../../app/assets/dormitor/comode/Kentuki/Comoda Kentuki.jpeg"
+import comodaKentuki_1 from "../../../../app/assets/dormitor/comode/Kentuki/Comoda Kentuki-2.jpeg"
+
+
+export const comodeProducts = [
+    {
+      id: 1,
+      name: "Comoda Geneva 1S2D",
+      images: [comodaGeneva1S2D, comodaGeneva1S2D_1, comodaGeneva1S2D_2],
+      description: "Este o comodă stilată cu un design cu forme ondulate care va decora dormitorul, camera de zi sau holul. Are un sertar și rafturi cu 2 uși. În loc de mânere pe uși, comoda are o adâncitură in forma de val pentru deschiderea ușilor, aceasta forma de val ce confera eleganta este punctul forte al întregii colectii Geneva.",
+      price: 480.00,
+      dimensiuni: {
+        lungime: 90,
+        inaltime: 107,
+        adancime: 40,
+      },
+      corpColor: "Stejar Sonoma Trufa+Wenge Magia, Gri Grafit +Stejar Craft Alb",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "geneva".toLowerCase(),
+    },
+    {
+      id: 2,
+      name: "Comoda Geneva 1S3D",
+      images: [comodaGeneva1S3D, comodaGeneva1S3D_1],
+      description: "Este o comodă stilată cu un design cu forme ondulate care va decora dormitorul, camera de zi sau holul.",
+      price: 580.00,
+      dimensiuni: {
+        lungime: 120,
+        inaltime: 106,
+        adancime: 40,
+      },
+      corpColor: "Stejar Sonoma Trufa+Wenge Magia",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "geneva1s3d".toLowerCase(),
+    },
+    {
+      id: 3,
+      name: "Comoda Geneva 3S",
+      images: [comodaGeneva3S, comodaGeneva3S_1, comodaGeneva3S_2],
+      description: "Comoda elegantă Geneva cu un design ondulat va fi un decor grozav pentru dormitorul, camera de zi sau holul dumneavoastră. Este destul de încăpător, confortabil și potrivit pentru orice interior, datorită culorii sale calde de trufe. Are trei sertare spațioase pentru hainele și lucrurile tale. Comoda poate fi completată cu un pat Geneva, care arată și stilat și are mai multe dimensiuni.",
+      price: 770.00,
+      dimensiuni: {
+        lungime: 120,
+        inaltime: 107,
+        adancime: 50,
+      },
+      corpColor: "Stejar Sonoma Trufa+Wenge Magia, Gri Grafit +Stejar Craft Alb",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "geneva3s".toLowerCase(),
+    },
+    {
+      id: 4,
+      name: "Comoda Flora",
+      images: [comodaFlora, comodaFlora_1, comodaFlora_2],
+      description: "Comoda Flora din material MDF. Tip de deschidere: cu balamale",
+      price: 620.00,
+      dimensiuni: {
+        lungime: 120,
+        inaltime: 97,
+        adancime: 40,
+      },
+      corpColor: "Alb, Sonoma Deschis",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "flora".toLowerCase(),
+    },
+    {
+      id: 5,
+      name: "Comoda Ares AS3",
+      images: [comodaAresAS3, comodaAresAS3_1],
+      description: "Comoda Ares din material MDF. Tip de deschidere: cu balamale. Muchii ABS, sistem de inchidere lenta",
+      price: 730.00,
+      dimensiuni: {
+        lungime: 104,
+        inaltime: 85,
+        adancime: 40,
+      },
+      corpColor: "-",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "ares".toLowerCase(),
+    },
+    {
+      id: 6,
+      name: "Comoda Ares AS6",
+      images: [comodaAresAS6, comodaAresAS6_1, comodaAresAS6_2],
+      description: "Comoda Ares din material MDF. Tip de deschidere: cu balamale. Muchii ABS, sistem de inchidere lenta",
+      price: 1030.00,
+      dimensiuni: {
+        lungime: 150,
+        inaltime: 85,
+        adancime: 40,
+      },
+      corpColor: "-",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "aresas6".toLowerCase(),
+    },
+    {
+      id: 7,
+      name: "Comoda Disney 900",
+      images: [comodaDisney900],
+      description: "Este un element de mobilier practic, estetic și versatil, adaptându-se nevoilor diverse ale utilizatorilor săi. Finisajele de calitate oferă un echilibru între utilitate și estetică. Cu dimensiunile sale bine proporționate, Comoda Disney se integrează perfect în diverse spații, de la dormitoare până la camere de joacă. Sertarele spațioase asigură suficient loc pentru a organiza și depozita haine, accesorii sau alte obiecte personale. Fiecare sertar este proiectat cu atenție la detalii pentru a asigura accesibilitate și funcționalitate.",
+      price: 530.00,
+      dimensiuni: {
+        lungime: 90,
+        inaltime: 90,
+        adancime: 39,
+      },
+      corpColor: "Grafit Gri + Stejar Craft Alb",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "disney".toLowerCase(),
+    },
+    {
+      id: 8,
+      name: "Comoda Disney 2D4S",
+      images: [comodaDisney2D4S, comodaDisney2D4S_1],
+      description: "Este un element de mobilier practic, estetic și versatil, adaptându-se nevoilor diverse ale utilizatorilor săi. Finisajele de calitate oferă un echilibru între utilitate și estetică. Cu dimensiunile sale bine proporționate, Comoda Disney se integrează perfect în diverse spații, de la dormitoare până la camere de joacă. Sertarele spațioase asigură suficient loc pentru a organiza și depozita haine, accesorii sau alte obiecte personale. Fiecare sertar este proiectat cu atenție la detalii pentru a asigura accesibilitate și funcționalitate.",
+      price: 710.00,
+      dimensiuni: {
+        lungime: 120,
+        inaltime: 90,
+        adancime: 39,
+      },
+      corpColor: "Grafit Gri + Stejar Craft Alb",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "disney2d4s".toLowerCase(),
+
+    },
+    {
+      id: 9,
+      name: "Comoda UK80",
+      images: [comodaUK80],
+      description: "Comoda UK80 din material MDF. Tip de deschidere: cu balamale. Muchii ABS, sistem de inchidere lenta",
+      price: 600.00,
+      dimensiuni: {
+        lungime: 80,
+        inaltime: 80,
+        adancime: 42,
+      },
+      corpColor: "-",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "uk80".toLowerCase(),
+    },
+    {
+      id: 10,
+      name: "Comoda UK120",
+      images: [comodaUK120, comodaUK120_1],
+      description: "Comoda UK120 din material MDF. Tip de deschidere: cu balamale. Muchii ABS, sistem de inchidere lenta",
+      price: 700.00,
+      dimensiuni: {
+        lungime: 120,
+        inaltime: 80,
+        adancime: 42,
+      },
+      corpColor: "Sonoma, Alb",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "uk120".toLowerCase(),
+    },
+    {
+      id: 11,
+      name: "Comoda Nika",
+      images: [comodaNika],
+      description: "Comoda Nika din material MDF. Tip de deschidere: cu balamale. Muchii ABS, sistem de inchidere lenta",
+      price: 610.00,
+      dimensiuni: {
+        lungime: 70,
+        inaltime: 90,
+        adancime: 45,
+      },
+      corpColor: "Sonoma",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "nika".toLowerCase(),
+    },
+    {
+      id: 12,
+      name: "Comoda Venera 3S",
+      images: [comodaVenera3S, comodaVenera3S_1],
+      description: "Comoda elegantă „Venera” vă va decora apartamentul sau casa. Este suficient de încăpător, confortabil și potrivit pentru orice interior. Are trei cutii încăpătoare pentru hainele și lucrurile tale.",
+      price: 630.00,
+      dimensiuni: {
+        lungime: 100,
+        inaltime: 92,
+        adancime: 35,
+      },
+      corpColor: "",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "venera3s".toLowerCase(),
+      
+    },
+    {
+      id: 13,
+      name: "Comoda Venera 3S2D",
+      images: [comodaVenera3S2D, comodaVenera3S2D_1, comodaVenera3S2D_2],
+      description: "Comoda „Venera” va fi un plus excelent pentru camera de zi, dormitor, hol sau creșă. Are un design elegant care se potrivește aproape oricărui interior. Spațios și ușor de utilizat. Acest model va completa casa ta și va deveni un lucru indispensabil.Material de producție - PAL, ghidaje - role.",
+      price: 650.00,
+      dimensiuni: {
+        lungime: 120,
+        inaltime: 92,
+        adancime: 40,
+      },
+      corpColor: "",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "venera3s2d".toLowerCase(),
+    },
+    {
+      id: 14,
+      name: "Comoda TV Bollo",
+      images: [comodaTVBollo],
+      description: "Comoda TV Bollo din material MDF. Tip de deschidere: cu balamale. Muchii ABS, sistem de inchidere lenta",
+      price: 600.00,
+      dimensiuni: {
+        lungime: 184,
+        inaltime: 55,
+        adancime: 40,
+      },
+      corpColor: "Stejar Artizan + Alb lucios",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "tvbollo".toLowerCase(),
+    },
+    {
+      id: 15,
+      name: "Comoda Alma 1U3S",
+      images: [comodaAlma1U3S, comodaAlma1U3S_1, comodaAlma1U3S_2],
+      description: "PAL Melaminat, cant ABS, manere satinate, picioare PVC, glisiera cu sina",
+      price: 470.00,
+      dimensiuni: {
+        lungime: 70,
+        inaltime: 82,
+        adancime: 40,
+      },
+      corpColor: "Stejar Sonoma Deschis, Pin Antichizat, Sonoma Inchis + Deschis",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "alma1u3s".toLowerCase(),
+    },
+    {
+      id: 16,
+      name: "Comoda Alma 2U3S",
+      images: [comodaAlma2U3S, comodaAlma2U3S_1, comodaAlma2U3S_2, comodaAlma2U3S_3],
+      description: "PAL Melaminat, cant ABS, manere satinate, picioare PVC, glisiera cu sina",
+      price: 610.00,
+      dimensiuni: {
+        lungime: 105,
+        inaltime: 82,
+        adancime: 40,
+      },
+      corpColor: "Stejar Sonoma Deschis, Pin Antichizat, Sonoma Inchis + Deschis",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "alma2u3s".toLowerCase(),
+    },
+    {
+      id: 17,
+      name: "Comoda Alma 4S",
+      images: [comodaAlma4S, comodaAlma4S_1, comodaAlma4S_2, comodaAlma4S_3],
+      description: "PAL Melaminat, cant ABS, manere satinate, picioare PVC, glisiera cu sina",
+      price: 650.00,
+      dimensiuni: {
+        lungime: 50,
+        inaltime: 100,
+        adancime: 40,
+      },
+      corpColor: "Stejar Sonoma Deschis, Pin Antichizat, Sonoma Inchis + Deschis",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "alma4s".toLowerCase(),
+    },
+    {
+      id: 18,
+      name: "Comoda Luxor",
+      images: [comodaLuxor, comodaLuxor_1, comodaLuxor_2],
+      description: "PAL Melaminat, cant ABS, manere satinate, picioare PVC, glisiera cu sina",
+      price: 690.00,
+      dimensiuni: {
+        lungime: 50,
+        inaltime: 100,
+        adancime: 40,
+      },
+      corpColor: "Pin Antichizat, Sonoma Inchis + Deschis",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "luxor".toLowerCase(),
+    },
+    {
+      id: 19,
+      name: "Comoda Kentuki",
+      images: [comodaKentuki, comodaKentuki_1],
+      description: "Trăsăturile clasice ale colecţiei sunt scoase în evidenţă de mânerele cu aspect antichizat, picioarele elegante şi elementele decorative care se regăsesc pe fronturi. Finisajul este alb cu o mică patină, care crează o atmosferă elegantă.",
+      price: 690.00,
+      dimensiuni: {
+        lungime: 155,
+        inaltime: 90,
+        adancime: 45,
+      },
+      corpColor: "Alb",
+      faceColor: "-",
+      category: "comode",
+      slug: "comode-" + "kentuki".toLowerCase(),
+
+    }
+
+  ]

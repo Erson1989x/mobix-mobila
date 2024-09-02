b0d06805620551da6c373175f59f395f67f89808
Erhard Nagy <Erson1989x@gmail.com>
2024-09-02 15:47:09 +0300

    in-progress

--- app/(front)/products/[slug]/page.js
+++ app/(front)/products/[slug]/page.js
@@ -6,6 +6,7 @@ import { dulapuriProducts } from "../../../../library/categories/dormitor/dulapu
 import { noptiereProducts } from "../../../../library/categories/dormitor/noptiere/noptiereProducts";
 import { paturiProducts } from "@/library/categories/dormitor/paturi/paturiProducts";
 import { comodaProducts } from "@/library/categories/dormitor/comode/comodeProducts";
+import { salteleProducts } from "@/library/categories/dormitor/saltele/salteleProducts";
 import ImageGallery from "@/components/ImageGallery/ImageGallery";
 const ProductPage = () => {
   const pathname = usePathname();
@@ -24,49 +25,138 @@ const ProductPage = () => {
     (product) => product.slug === slug
   );
 
-  const product =
-    dulapuriProduct ?? noptiereProduct ?? paturiProduct ?? comodaProduct;
-
-  if (!product) {
-    return <div>Product not found</div>;
-  }
-
-  return (
-    <div className="md:p-8 pt-16 md:pt-20">
-      <div className="w-36 h-9">
-        <Link
-          className="cursor-pointer border border-0 p-2 rounded bg-white text-black shadow-lg mt-4"
-          href={`/dormitor/${product.category}`}
-        >
-          Inapoi la produse
-        </Link>
-      </div>
-      <div className="min-h-full p-4 md:p-8 pt-16 md:pt-20 grid grid-cols-1 sm:grid-cols-2 gap-8">
-        <div className="card shadow-lg p-2 rounded">
-          <ImageGallery images={product.images} />
-        </div>
-        <div className="card shadow-lg p-2 rounded">
-          <h1 className="text-2xl font-bold mb-2">{product.name}</h1>
-          <p className="mb-4">{product.description}</p>
-          <p className="mb-4 text-red-600">{product.price} - RON</p>
-          <p className="mb-4">
-            Dimensiuni:{" "}
-            {[
-              product.dimensiuni?.latime,
-              product.dimensiuni?.lungime,
-              product.dimensiuni?.inaltime,
-              product.dimensiuni?.adancime,
-            ]
-              .filter((dim) => dim)
-              .join(" x ")}
-          </p>
-          <p className="mb-4">
-            Culoare: Corp - {product.corpColor}, Față - {product.faceColor}
-          </p>
-        </div>
-      </div>
-    </div>
+  const salteleProduct = salteleProducts?.find(
+    (product) => product.slug === slug
   );
+
+  const product =
+    dulapuriProduct ??
+    noptiereProduct ??
+    paturiProduct ??
+    comodaProduct ??
+    salteleProduct;
+
+  if (product && product === salteleProduct) {
+    return (
+      <div className="md:p-8 pt-16 md:pt-20">
+        <div className="w-36 h-9">
+          <Link
+            className="cursor-pointer border border-0 p-2 rounded bg-white text-black shadow-lg mt-4"
+            href={`/dormitor/${product.category}`}
+          >
+            Inapoi la produse
+          </Link>
+        </div>
+        <div className="min-h-full p-4 md:p-8 pt-16 md:pt-20 grid grid-cols-1 sm:grid-cols-2 gap-8">
+          <div className="card shadow-lg p-2 rounded">
+            <ImageGallery images={product.images} />
+          </div>
+          <div className="card shadow-lg p-2 rounded">
+            <h1 className="text-2xl font-bold mb-2">{product.name}</h1>
+            <div className="mb-4">
+              {product.description.split(/[;.]/).map((line, index) => (
+                <p className="mb-2" key={index}>
+                  {line.trim()}
+                </p>
+              ))}
+            </div>
+            {product.prices.variantaOne && (
+              <p className="mb-4">
+                Varianta 90 x 200:{" "}
+                <span className="text-red-600 font-bold">
+                  {product.prices.variantaOne}
+                </span>{" "}
+                - RON
+              </p>
+            )}
+            {product.prices.variantaOneTwo && (
+              <p className="mb-4">
+                Varianta 120 x 200:{" "}
+                <span className="text-red-600 font-bold">
+                  {product.prices.variantaOneTwo}
+                </span>{" "}
+                - RON
+              </p>
+            )}
+            {product.prices.variantaTwo && (
+              <p className="mb-4">
+                Varianta 140 x 200:{" "}
+                <span className="text-red-600 font-bold">
+                  {product.prices.variantaTwo}
+                </span>{" "}
+                - RON
+              </p>
+            )}
+            {product.prices.variantaThree && (
+              <p className="mb-4">
+                Varianta 160 x 200:{" "}
+                <span className="text-red-600 font-bold">
+                  {product.prices.variantaThree}
+                </span>{" "}
+                - RON
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="md:p-8 pt-16 md:pt-20">
+        <div className="w-36 h-9">
+          <Link
+            className="cursor-pointer border border-0 p-2 rounded bg-white text-black shadow-lg mt-4"
+            href={`/dormitor/${product.category}`}
+          >
+            Inapoi la produse
+          </Link>
+        </div>
+        <div className="min-h-full p-4 md:p-8 pt-16 md:pt-20 grid grid-cols-1 sm:grid-cols-2 gap-8">
+          <div className="card shadow-lg p-2 rounded">
+            <ImageGallery images={product.images} />
+          </div>
+          <div className="card shadow-lg p-2 rounded">
+            <h1 className="text-2xl font-bold mb-2">{product.name}</h1>
+            <p className="mb-4">{product.description}</p>
+            {product.prices &&
+            product.prices.cuSomiera &&
+            product.prices.somieraRabatabila ? (
+              <p className="mb-4">
+                Varianta cu somiera:{" "}
+                <span className="text-red-600 font-bold">
+                  {product.prices.cuSomiera}
+                </span>{" "}
+                - RON Somiera rabatabila:{" "}
+                <span className="text-red-600 font-bold">
+                  {product.prices.somieraRabatabila}
+                </span>{" "}
+                - RON
+              </p>
+            ) : null}
+            <p className="mb-4 text-red-600 font-bold">{product.price} - RON</p>
+            <p className="mb-4">
+              Dimensiuni:{" "}
+              {[
+                product.dimensiuni?.latime &&
+                  `Lățime: ${product.dimensiuni.latime} cm`,
+                product.dimensiuni?.lungime &&
+                  `Lungime: ${product.dimensiuni.lungime} cm`,
+                product.dimensiuni?.inaltime &&
+                  `Înălțime: ${product.dimensiuni.inaltime} cm`,
+                product.dimensiuni?.adancime &&
+                  `Adâncime: ${product.dimensiuni.adancime} cm`,
+              ]
+                .filter((dim) => dim)
+                .join(" x ")}
+            </p>
+            <p className="mb-4">
+              Culoare: Corp - {product.corpColor}, Față - {product.faceColor}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default ProductPage;
---
+++ app/dormitor/saltele/page.js
@@ -0,0 +1,81 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { salteleProducts } from '../../../library/categories/dormitor/saltele/salteleProducts'
+import { useState } from 'react'
+
+const Saltele = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 8;
+    const totalPages = Math.ceil(salteleProducts.length / itemsPerPage);
+  
+    const productsToDisplay = salteleProducts.slice(
+      (currentPage - 1) * itemsPerPage,
+      currentPage * itemsPerPage
+    );
+
+  return (
+    <div className="h-full p-4 md:p-8 pt-16 md:pt-20">
+      <h1 className="text-3xl font-bold mb-8 text-center">Saltele</h1>
+      <Link
+        className="cursor-pointer border border-0 p-2 rounded bg-white text-black shadow-lg mt-4"
+        href="/dormitor"
+      >
+        Inapoi la produse
+      </Link>
+      <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 mt-8">
+        {productsToDisplay.map((product) => (
+          <div
+            key={product.name}
+            className="card shadow-lg p-2 border border-gray-300 rounded"
+          >
+            <h2 className="font-bold text-center mb-2">{product.name}</h2>
+            <div className="flex justify-center mb-2">
+              <Link href={`/products/${product.slug}`} key={product.slug}>
+                <Image
+                  src={
+                    Array.isArray(product.images)
+                      ? product.images[0].src
+                      : product.images.src
+                  }
+                  width={
+                    Array.isArray(product.images)
+                      ? product.images[0].width
+                      : product.images.width
+                  }
+                  height={
+                    Array.isArray(product.images)
+                      ? product.images[0].height
+                      : product.images.height
+                  }
+                  priority={true}
+                  alt=""
+                  className="w-full h-60"
+                />
+              </Link>
+            </div>
+            <p className="text-center mb-2">Pentru Detalii click pe poza</p>
+            <p className="text-center text-red-600">{product.price} - RON</p>
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center mt-8">
+        {Array.from({ length: totalPages }, (_, i) => (
+          <Link
+            key={i + 1}
+            href="#"
+            onClick={() => setCurrentPage(i + 1)}
+            className={`px-4 py-2 cursor-pointer border border-0 rounded bg-white text-black shadow-md animate-pulse ${
+              currentPage === i + 1 ? "text-black" : "bg-gray-200"
+            }`}
+          >
+            {i + 1}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Saltele

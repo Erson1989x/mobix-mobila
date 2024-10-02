359e20865e87e25c5063e5b56b7967c4ead00697
Erhard Nagy <Erson1989x@gmail.com>
2024-10-02 19:05:32 +0300

    in-progress

--- components/AboutUs/AboutUs.jsx
+++ components/AboutUs/AboutUs.jsx
@@ -6,15 +6,15 @@ const AboutUs = () => {
 
   return (
     <section id="about-us" className="w-full h-auto flex flex-col items-center justify-center">
-    <h2 className="mt-8 text-3xl"> Despre noi </h2>
+    <h2 className="mt-8 text-3xl lg:text-4xl"> Despre noi </h2>
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8 md:gap-12">
     {aboutUsDataSections.map(({ title, description, content }, index) => (
           <div key={index} className="card">
-            <h4 className="text-lg font-bold text-center">{title}</h4>
+            <h4 className="text-lg font-bold text-center lg:text-xl">{title}</h4>
             {description ? (
-              <p className="mt-2 text-md text-center">{description}</p>
+              <p className="mt-2 text-md text-center lg:text-lg">{description}</p>
             ) : (
-              <p className="mt-2 text-md text-center">{content}</p>
+              <p className="mt-2 text-md text-center lg:text-lg">{content}</p>
             )}
           </div>
         ))}
--- components/ProduseServicii/ProduseServicii.jsx
+++ components/ProduseServicii/ProduseServicii.jsx
@@ -9,18 +9,27 @@ const ProduseServicii = () => {
       id="products"
       className="w-full h-auto flex flex-col items-center justify-center mt-4"
     >
-      <h2 className="mt-8 text-3xl"> Produse & Servicii </h2>
-      <p className="mt-4 text-lg text-center">
+      <h2 className="mt-8 text-3xl lg:text-4xl"> Produse & Servicii </h2>
+      <p className="mt-4 text-lg text-center lg:text-2xl">
         {" "}
-        Alegeți produsele care să se potrivească stilului și personalității dvs.
-        Indiferent dacă preferați un aspect simplu și elegant sau unul mai plin
-        de viață și culoare, avem o opțiune pentru tine. Fie că alegi materiale
-        naturale sau sintetice, culori deschise sau întunecate, noi avem ceea ce
-        căutați.
+        La MobiX, suntem dedicați să oferim mobilier de înaltă calitate, care
+        îmbină funcționalitatea cu designul modern și confortul. De-a lungul
+        anilor, ne-am consolidat prezența pe piață printr-o selecție variată de
+        piese de mobilier, atent alese pentru a satisface gusturile și nevoile
+        fiecărui client. De la mobilier clasic, până la modele contemporane, ne
+        propunem să aducem în casele clienților noștri soluții estetice și
+        durabile. Prioritatea noastră este să asigurăm o experiență de cumpărare
+        plăcută, cu produse de calitate, servicii de consultanță profesioniste
+        și livrare rapidă. Fie că doriți să amenajați un spațiu de locuit sau
+        unul de lucru, la MobiX veți găsi tot ce aveți nevoie pentru a
+        transforma orice încăpere într-un loc primitor și funcțional.
       </p>
       <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 mt-8 justify-center">
         {produseItems.map(({ title, image, url }, index) => (
-          <div key={index} className="card relative border border-1 border-black shadow-md overflow-hidden">
+          <div
+            key={index}
+            className="card relative border border-1 border-black shadow-md overflow-hidden"
+          >
             <Image
               src={image}
               alt="produs"
